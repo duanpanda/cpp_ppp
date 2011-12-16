6e3de42ac780155baf17e507bdf1a61c80cdba33
Ryan Duan <duanpanda@gmail.com>
2011-12-16 11:07:49 +0800

    Add try3_3.cpp.  Read name and age.

---
+++ ch1/drill/try3_3.cpp
@@ -0,0 +1,12 @@
+// read name and age
+#include "../../std_lib_facilities.h"
+
+int main()
+{
+  cout << "Please enter your first name and age\n";
+  string first_name = "???";
+  double age = -1.0;
+  cin >> first_name >> age;
+  cout << "Hello, " << first_name << " (age " << age * 12 << " months)\n";
+  return 0;
+}
