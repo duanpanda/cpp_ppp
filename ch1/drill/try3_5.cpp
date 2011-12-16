134e87c538e066e332db7f027fe8c0c864774767
Ryan Duan <duanpanda@gmail.com>
2011-12-16 17:36:33 +0800

    Add try3_5.  Repeated word detection program.

---
+++ ch1/drill/try3_5.cpp
@@ -0,0 +1,13 @@
+// delete repeated words
+#include "../../std_lib_facilities.h"
+
+int main()
+{
+  string previous = " ";        // previous word; initialize to "not a word"
+  string current;               // current word
+  while (cin >> current) {      // read a stream of words
+    if (previous == current)    // check if the word is the same as last
+      cout << "repeated word: " << current << '\n';
+    previous = current;
+  }
+}
